b130625427bf3c2af89a9832a5a76508b5a4b7bf
FahmiBahtiar <fahmibahtiar76@gmail.com>
2025-02-12 14:40:23 +0700

    update vercel config and add any more feature

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 const __filename = path.resolve();
 const __dirname = path.dirname(__filename);
 
-app.set("views", path.join(__dirname, "/botlab/views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("main");
