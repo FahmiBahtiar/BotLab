732faecbe6d34d18fe0da019aeba837648759834
FahmiBahtiar <fahmibahtiar76@gmail.com>
2025-02-21 11:16:49 +0700

    fix database and update feature

--- index.js
+++ index.js
@@ -39,9 +39,10 @@ bot.setMyCommands(commands)
         console.error('[LOG] Gagal mendaftarkan perintah bot:', err);
     });
 
-const whitelist = ['1105365521'];
+const whitelist = ['1105365521','1324267240'];
 let laboran = [
     { chatId: '1105365521', name: 'Fahmi' }
+    { chatId: '1324267240', name: 'Faqih' }
 ];
 
 const checkWhitelist = (chatId) => {
